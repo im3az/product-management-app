9c7abe32a2cd79ad9e9a38dd4d96c59221c0c6ff
Imtiaz <im3azchowdhury@gmail.com>
2024-05-24 00:12:44 +0600

    all product fetch route added

--- src/modules/products/product.controller.ts
+++ src/modules/products/product.controller.ts
@@ -2,17 +2,34 @@ import { Request, Response } from 'express';
 import { ProductServices } from './product.service';
 
 const createProduct = async (req: Request, res: Response) => {
-  const productData = req.body;
+  try {
+    const productData = req.body;
+    const result = await ProductServices.createProduct(productData);
 
-  const result = await ProductServices.createProduct(productData);
+    res.json({
+      success: true,
+      message: 'Product created successfully!',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-  res.json({
-    success: true,
-    message: 'Product created successfully!',
-    data: result,
-  });
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProducts();
+    res.status(200).json({
+      success: true,
+      message: 'Products fetched successfully!',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const ProductControllers = {
   createProduct,
+  getAllProducts,
 };
--- src/modules/products/product.route.ts
+++ src/modules/products/product.route.ts
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.post('/', ProductControllers.createProduct);
 
+router.get('/', ProductControllers.getAllProducts);
+
 export const ProductRoutes = router;
